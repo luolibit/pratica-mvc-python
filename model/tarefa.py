a06daaa0ab7627ccb18d67e7d57a8b22061a6a3a
luolibit <horadalu1@gmail.com>
2025-02-11 16:25:57 -0300

    Update tarefa.py

--- model/tarefa.py
+++ model/tarefa.py
@@ -1,4 +1,4 @@
-from model.database import Database
+from database import Database
  
 class Tarefa:
     def __init__(self, id, titulo, data_conclusao):
@@ -24,10 +24,23 @@ class Tarefa:
         sql = 'SELECT id, titulo, data_conclusao FROM tarefa'
         tarefas = db.consultar(sql)
         db.desconectar()
-        # return tarefas if tarefas else []
-   
-        if tarefas:
-            return tarefas
-        else:
-            return[]
+        return tarefas if tarefas else []
+        # if tarefas:
+        #     return tarefas
+        # else:
+        #     return[]
  
+    def apagarTarefa(self):
+        """Apaga uma tarefa cadastrada no banco de dados."""
+        db = Database()
+        db.conectar()
+ 
+        sql = 'DELETE FROM tarefa WHERE id = %s'
+        params = (self.id) # Precisa passar como tupla? (a, b, c, ...) SIM!
+        db.executar(sql, params)
+        db.desconectar()
+ 
+ 
+# Área 51
+tarefa = Tarefa(1, 'Teste de tarefa', None)
+print(tarefa)
